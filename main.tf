01d04153d93d862e8813f9e58741d9f4f2b88f87
kennedyuc <chukwukakennedyuche@gmail.com>
2022-10-20 12:25:42 -0700

    file updates

--- main.tf
+++ main.tf
@@ -38,11 +38,11 @@ resource "kubectl_manifest" "cert_manager" {
 resource "helm_release" "cert_manager" {
   depends_on       = [kubectl_manifest.cert_manager]
   name             = "cert-manager"
-  namespace        = "cert-manager"
-  create_namespace = var.create_namespace
+  namespace        = var.cert_namespace
+  create_namespace = var.helm_create_namespace
   chart            = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  version          = "v1.9.1"
+  repository       = var.cert_chart_repo
+  version          = var.cert_chart_version
   values = [
     file("./helm-values/cert-manager-values.yaml")]
   atomic           = var.enable_atomic
--- variables.tf
+++ variables.tf
@@ -1,11 +1,29 @@
-variable "create_namespace" {
-    type = bool
-    default = False
+variable "helm_create_namespace" {
+    type        = bool
+    default     = False
     description = "Specify the whether namespace is created by Helm"
 }
 
 variable "enable_atomic" {
-    type = bool
-    default = False
+    type        = bool
+    default     = False
     description = "Specify the whether atomic is enabled in Helm"
+}
+
+variable "cert_chart_version" {
+    type        = string
+    default     = "v1.9.1"
+    description = "Specify the version of Helm"
+}
+
+variable "cert_chart_repo" {
+    type        = string
+    default     = "https://charts.jetstack.io"
+    description = "helm chart repo"
+}
+
+variable "cert_namespace" {
+    type        = string
+    default     = "cert-manager"
+    description = "cert-manager namespace"
 }
